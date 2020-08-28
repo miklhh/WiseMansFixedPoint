6b4c70f02b264dbff63600643fe430a0bc7dd3f5
Mikael Henriksson <mikael.henriksson@liu.se>
2020-08-28 15:32:29 +0200

    Testing some evil undefined code...

--- FixedPoint.h
+++ FixedPoint.h
@@ -95,9 +95,10 @@ namespace detail
      */
     static inline int ilog2_fast(double d)
     {
-        int result;
-        std::frexp(d, &result);
-        return result-1;
+        //int result;
+        //std::frexp(d, &result);
+        //return result-1;
+        return ((reinterpret_cast<unsigned long long&>(d) >> 52) & 0x7ff) - 1023;
     }
 
     /*
